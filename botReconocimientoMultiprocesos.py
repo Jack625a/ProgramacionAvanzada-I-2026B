9487fd4f0618814d54fa5405cab6f0ab76efe294
Jack625a <ktfut15@gmail.com>
2026-03-25 17:44:16 -0400

    bOTProcesamiento Imagen

---
+++ botReconocimientoMultiprocesos.py
@@ -0,0 +1,46 @@
+import cv2
+import os
+import numpy as np
+from multiprocessing import Process,Queue
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+tokenTelegram="8748613397:AAGopUqSUr8Y0EwQXURh9iu1DoTQmuuLEzI"
+
+def procesar(colaEntrada,colaSalida):
+    while True:
+        rutaImagen=colaEntrada.get()
+        if rutaImagen is None:
+            break
+        img=cv2.imread(rutaImagen)
+        conversionGris=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        bordes=cv2.Canny(conversionGris,100,100)
+        salida=rutaImagen.replace(".jpg","_procesado.jpg")
+        cv2.imwrite(salida,bordes)
+        colaSalida.put(salida)
+
+colaEntrante=Queue()
+colaSaliente=Queue()
+
+async def recibirImage(update:Update,context:ContextTypes.DEFAULT_TYPE):
+    foto=update.message.photo[-1]
+    archivo=await foto.get_file()
+    ruta=f"image_{foto.file_id}.jpg"
+    await archivo.download_to_drive(ruta)
+    colaEntrante.put(ruta)
+    resultado=colaSaliente.get()
+    await update.message.reply_photo(photo=open(resultado,"rb"))
+    os.remove(ruta)
+    os.remove(resultado)
+
+def main():
+    proceso=Process(target=procesar,args=(colaEntrante,colaSaliente))
+    proceso.start()
+
+    app=ApplicationBuilder().token(tokenTelegram).build()
+    app.add_handler(MessageHandler(filters.PHOTO, recibirImage))
+    print("Bot incializado")
+    app.run_polling()
+
+if __name__=="__main__":
+    main()
